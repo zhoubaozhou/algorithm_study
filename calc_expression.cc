531611aa22fc00c356182ebdb10474f890fbaad3
zhoubaozhou <zhoubaozhou@gmail.com>
2013-10-15 20:47:45 +0800

    support bracket

--- calc_expression.cc
+++ calc_expression.cc
@@ -13,6 +13,7 @@ typename T::value_type pop(T& stack) {
 int priority(int op) {
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
+    if (op == '(') return 0;
     assert(0);
 }
 
@@ -31,6 +32,17 @@ int calc(char op, int x, int y) {
     }
 }
 
+void calc_expression_in_bracket(std::stack<int>& num_stack, std::stack<char>& op_stack) {
+    while (op_stack.top() != '(') {
+        int y = pop(num_stack);
+        int x = pop(num_stack);
+        char op = pop(op_stack);
+        num_stack.push(calc(op, x, y)); 
+    }
+    assert(op_stack.top() == '(');
+    pop(op_stack);
+}
+
 int calc_expression(const std::string& expression) {
     std::stack<int> num_stack;
     std::stack<char> op_stack;
@@ -46,19 +58,24 @@ int calc_expression(const std::string& expression) {
             num_stack.push(atoi(num_str.c_str()));
             num_str.clear();
         }
-        if (op_stack.size() == 0 || priority(c) >= priority(op_stack.top())) {
-            op_stack.push(expression[i]);
+        if (c == ')') {
+            calc_expression_in_bracket(num_stack, op_stack);
             continue;
         }
-        while (op_stack.size() > 0) {
-            if (priority(c) <= priority(op_stack.top())) {
-                break;
-            }
+        if (c == '(' || op_stack.size() == 0 || priority(c) >= priority(op_stack.top())) {
+            op_stack.push(c);
+            continue;
+        }
+        while (op_stack.size() > 0 && priority(c) < priority(op_stack.top())) {
             int op = pop(op_stack);
             int y = pop(num_stack);
             int x = pop(num_stack);
             num_stack.push(calc(op, x, y));
         }
+        op_stack.push(c);
+    }
+    if (num_str.length()) {
+        num_stack.push(atoi(num_str.c_str()));
     }
     while (op_stack.size() > 0) {
         int op = pop(op_stack);
@@ -66,11 +83,13 @@ int calc_expression(const std::string& expression) {
         int x = pop(num_stack);
         num_stack.push(calc(op, x, y));
     }
+    assert(num_stack.size() == 1);
+    assert(op_stack.size() == 0);
     return num_stack.top();
 }
 
 int main() {
-    std::cout << calc_expression("5*1+2+3*4+5-2/1") << std::endl;
+    std::cout << calc_expression("(5*1)+(2+3)*(4+5-2/1)") << std::endl;
     return 0;
 }
 
